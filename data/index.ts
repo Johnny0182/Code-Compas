ad133d3389bc580d5409a71b6d9ad137623fe66d
johnny0182 <johnny.leyva182@gmail.com>
2025-09-24 03:25:48 -0700

    1

---
+++ data/index.ts
@@ -0,0 +1,169 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 2,
+    title: "Any timezone. Any industry. Always available 🤠",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    description: "Mission Statement (Code Compas 🤠):",
+    title:
+      "We close the technology gap with affordable, scalable, and beautifully designed solutions. We build custom apps, software, automations, and AI agents. Helping entrepreneurs, small businesses, nonprofits, and mission-driven organizations to thrive.",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    descClassName:
+      "mt-2 text-sm sm:text-[15px] md:text-base lg:text-lg leading-6 sm:leading-7 lg:leading-8 text-white/90 max-w-[68ch] [text-wrap:pretty] hyphens-auto",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 5,
+    title:
+      "Currently serving 20+ businesses. If you can think it, we can code it. Custom apps, software, websites, automations, and digital marketing.",
+    description: "Code Compas 🤠 | A full service development agency",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Free consultation. Fair pricing, No BS!",
+    description: "Dream it. We'll build it.",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "3D Solar System Planets to Explore",
+    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "https://ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Yoom - Video Conferencing App",
+    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/https://www.johnnyleyva.com",
+  },
+  {
+    id: 3,
+    title: "AI Image SaaS - Canva Application",
+    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Code Compas was an absolute pleasure. The professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. The teams enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Code Compas is the ideal partners.",
+    name: "Michael Johnson",
+    title: "Director of Stark Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Code Compas was an absolute pleasure. The professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. The teams enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Code Compas is the ideal partners.",
+    name: "Michael Johnson",
+    title: "Director of Stark Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Code Compas was an absolute pleasure. The professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. The teams enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Code Compas is the ideal partners.",
+    name: "Michael Johnson",
+    title: "Director of Stark Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Code Compas was an absolute pleasure. The professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. The teams enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Code Compas is the ideal partners.",
+    name: "Michael Johnson",
+    title: "Director of Stark Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Code Compas was an absolute pleasure. The professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. The teams enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Code Compas is the ideal partners.",
+    name: "Michael Johnson",
+    title: "Director of Stark Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
