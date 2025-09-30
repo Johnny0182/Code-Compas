5b6cec40e750a27555ea67661a35eb396a18f3c3
johnny0182 <johnny.leyva182@gmail.com>
2025-09-29 19:48:16 -0700

    Websites and Marketing in English & Español

--- data/index.ts
+++ data/index.ts
@@ -9,26 +9,15 @@ export const gridItems = [
   {
     id: 5,
     title:
-      "Currently serving 20+ businesses. If you can think it, we can code it. Custom apps, software, websites, automations, and digital marketing.",
-    description: "Code Compas 🤠 | A full service development agency",
+      "From custom apps and websites to workflow automation and marketing, we bring your digital vision to life.",
+    description: "We are Code Compas 🤠",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
-    img: "/b5.svg",
-    spareImg: "/grid.svg",
-  },
-  {
-    id: 3,
-    description: "Mission Statement:",
-    title:
-      "We build custom digital solutions for businesses, nonprofits, and anyone with a vision. We love our community, keep pricing fair, and help organizations get powerful tech without breaking the bank.",
-    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
-    imgClassName: "",
-    titleClassName: "justify-center md:justify-start lg:justify-center",
     descClassName:
       "mt-2 text-sm sm:text-[15px] md:text-base lg:text-lg leading-6 sm:leading-7 lg:leading-8 text-white/90 max-w-[68ch] [text-wrap:pretty] hyphens-auto",
-    img: "",
-    spareImg: "",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
   },
   {
     id: 2,
@@ -41,9 +30,22 @@ export const gridItems = [
     spareImg: "",
   },
   {
+    id: 3,
+    description: "Websites and Marketing in English & Español 🇺🇸🇲🇽",
+    title:
+      "We speak English and Spanish. Helping you win jobs, earn trust, and grow your business.",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    descClassName:
+      "mt-2 text-sm sm:text-[15px] md:text-base lg:text-lg leading-6 sm:leading-7 lg:leading-8 text-white/90 max-w-[68ch] [text-wrap:pretty] hyphens-auto",
+    img: "",
+    spareImg: "",
+  },
+  {
     id: 6,
-    title: "Free consultation. Fair pricing. No fluff.",
-    description: "You dream it, and we'll build it.",
+    title: "Book a free call to discuss custom projects 📞",
+    description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
     titleClassName: "justify-center md:max-w-full max-w-60 text-center",
